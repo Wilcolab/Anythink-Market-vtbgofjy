fe046489484063c3ea89abc37f14a3c0d70f238c
Aman Singh <151001715+Amansingh0807@users.noreply.github.com>
2025-10-12 07:46:39 +0000

    Add modulo operation to calculator controller

--- api/controller.js
+++ api/controller.js
@@ -17,6 +17,7 @@ exports.calculate = function(req, res) {
     'multiply': function(a, b) { return a * b },
     'divide':   function(a, b) { return a / b },
     'power':    function(a, b) { return Math.pow(a, b) },
+    'modulo':   function(a, b) { return a % b },
   };
 
   if (!req.query.operation) {
