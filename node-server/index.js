58f2dac5d32911b55db9727f27809770a1a61622
Aman Singh <151001715+Amansingh0807@users.noreply.github.com>
2025-10-13 07:23:31 +0000

    Add root endpoint to Node.js server for health check

--- node-server/index.js
+++ node-server/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 8001;
 
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
 app.listen(PORT, () => {
   console.log(`Node.js server listening on port ${PORT}`);
 });
