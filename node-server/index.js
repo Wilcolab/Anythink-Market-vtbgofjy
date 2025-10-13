46f4d216084399d38a9e2a9ea9abd45cc47550b3
Aman Singh <151001715+Amansingh0807@users.noreply.github.com>
2025-10-13 08:48:27 +0000

    Add root endpoint to Node.js server for / with Hello, world! response

--- node-server/index.js
+++ node-server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-// No endpoints
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 app.listen(8001, () => {
   console.log('Server listening on port 8001');
